65088861b4dfd1471c9ffd0ba7cc2a3414d212ba
Damien Delestienne <he201749@students.ephec.be>
2021-02-21 17:43:39 +0100

    maj script db

--- Docker/Postgre/creaDB.sql
+++ Docker/Postgre/creaDB.sql
@@ -1,11 +1,11 @@
 /****************Utilisateurs********************/
 
 CREATE TABLE utilisateurs(
-   id   	INTEGER       		NOT NULL,
-   nom    	VARCHAR(1000) 		NOT NULL,
-   prenom    	VARCHAR(1000) 		NOT NULL,                  
-   mail  	VARCHAR(1000) 		NOT NULL,
-   mdp          VARCHAR(1000)     	NOT NULL,
+   id   	SERIAL       		NOT NULL,
+   nom    	varchar(1000) 		NOT NULL,
+   prenom    	varchar(1000) 		NOT NULL,                  
+   mail  	varchar(1000) 		NOT NULL,
+   mdp          varchar(1000)     	NOT NULL,
    CONSTRAINT utilisateurs_pk PRIMARY KEY (id)
 );
 ALTER TABLE utilisateurs OWNER to "admin";
@@ -13,9 +13,9 @@ ALTER TABLE utilisateurs OWNER to "admin";
 /****************Boites********************/
 
 CREATE TABLE boites (
-   id   	INTEGER       		NOT NULL,                
-   mdp  	VARCHAR(1000) 		NOT NULL,
-   ip           VARCHAR(1000)     	NOT NULL,
+   id   	SERIAL       		NOT NULL,                
+   mdp  	varchar(1000) 		NOT NULL,
+   ip           varchar(1000)     	NOT NULL,
    CONSTRAINT boites_pk PRIMARY KEY (id)
 );
 ALTER TABLE boites OWNER to "admin";
@@ -23,13 +23,13 @@ ALTER TABLE boites OWNER to "admin";
 /****************Livraisons********************/
 
 CREATE TABLE livraisons (
-   id   	INTEGER       		NOT NULL,
-   utilisateur  INTEGER 		NOT NULL,
-   boite    	INTEGER 		NOT NULL,                  
-   dateDebut  	DATE 			NOT NULL,
-   dateFin  	DATE 			NOT NULL,
-   statut       INTEGER     		NOT NULL,
-   cadeau	INTEGER			NOT NULL,
+   id   	SERIAL       		NOT NULL,
+   utilisateur  integer 		NOT NULL,
+   boite    	integer 		NOT NULL,                  
+   dateDebut  	date 			NOT NULL,
+   dateFin  	date 			NOT NULL,
+   statut       integer     		NOT NULL,
+   cadeau	integer			NOT NULL,
    CONSTRAINT livraisons_pk PRIMARY KEY (id),
    CONSTRAINT fk_livraisons_utilisateurs FOREIGN KEY(utilisateur) REFERENCES utilisateurs(id) ON UPDATE NO ACTION ON DELETE CASCADE,
    CONSTRAINT fk_livraisons_boites FOREIGN KEY(boite) REFERENCES boites(id) ON UPDATE NO ACTION ON DELETE CASCADE
@@ -39,9 +39,9 @@ ALTER TABLE livraisons OWNER to "admin";
 /****************Acces********************/
 
 CREATE TABLE acces (
-   utilisateur  INTEGER       		NOT NULL,    
-   boite  	INTEGER       		NOT NULL,            
-   nom  	VARCHAR(1000) 		NOT NULL,
+   utilisateur  integer       		NOT NULL,    
+   boite  	integer       		NOT NULL,            
+   nom  	varchar(1000) 		NOT NULL,
    CONSTRAINT acces_pk PRIMARY KEY (utilisateur,boite),
    CONSTRAINT fk_acces_utilisateurs FOREIGN KEY(utilisateur) REFERENCES utilisateurs(id) ON UPDATE NO ACTION ON DELETE CASCADE,
    CONSTRAINT fk_acces_boites FOREIGN KEY(boite) REFERENCES boites(id) ON UPDATE NO ACTION ON DELETE CASCADE
