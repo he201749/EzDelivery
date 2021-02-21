0607280a7f99792d1157b5d9527f675ccae530eb
Damien Delestienne <he201749@students.ephec.be>
2021-02-21 15:46:59 +0100

    ajout du script db

--- Docker/Postgre/creaDB.sql
+++ Docker/Postgre/creaDB.sql
@@ -1,73 +1,49 @@
-/****************ACCESS********************/
-CREATE TABLE public.access
-(
-    door integer,
-    users integer,
-    tag character varying COLLATE pg_catalog."default",
-    nickname character varying COLLATE pg_catalog."default",
-    CONSTRAINT unique_doors_access UNIQUE (door, users),
-    CONSTRAINT unique_user_tag_name UNIQUE (users, tag, nickname),
-    CONSTRAINT access_users_fkey FOREIGN KEY (users)
-        REFERENCES public.users (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE CASCADE
-)
+/****************Utilisateurs********************/
 
-TABLESPACE pg_default;
+CREATE TABLE utilisateurs(
+   id   	INTEGER       		NOT NULL,
+   nom    	VARCHAR(1000) 		NOT NULL,
+   prenom    	VARCHAR(1000) 		NOT NULL,                  
+   mail  	VARCHAR(1000) 		NOT NULL,
+   mdp          VARCHAR(1000)     	NOT NULL,
+   CONSTRAINT utilisateurs_pk PRIMARY KEY (id)
+);
+ALTER TABLE utilisateurs OWNER to "admin";
 
-ALTER TABLE public.access
-    OWNER to "postgresArnaud";
+/****************Boites********************/
 
-/*********************DOOR*************************/
-CREATE TABLE public.door
-(
-    password character varying COLLATE pg_catalog."default",
-    status integer,
-    adresseip character varying COLLATE pg_catalog."default",
-    id integer NOT NULL DEFAULT nextval('door_id_seq'::regclass),
-    CONSTRAINT door_pkey PRIMARY KEY (id)
-)
+CREATE TABLE boites (
+   id   	INTEGER       		NOT NULL,                
+   mdp  	VARCHAR(1000) 		NOT NULL,
+   ip           VARCHAR(1000)     	NOT NULL,
+   CONSTRAINT boites_pk PRIMARY KEY (id)
+);
+ALTER TABLE boites OWNER to "admin";
 
-TABLESPACE pg_default;
+/****************Livraisons********************/
 
-ALTER TABLE public.door
-    OWNER to "postgresArnaud";
+CREATE TABLE livraisons (
+   id   	INTEGER       		NOT NULL,
+   utilisateur  INTEGER 		NOT NULL,
+   boite    	INTEGER 		NOT NULL,                  
+   dateDebut  	DATE 			NOT NULL,
+   dateFin  	DATE 			NOT NULL,
+   statut       INTEGER     		NOT NULL,
+   cadeau	INTEGER			NOT NULL,
+   CONSTRAINT livraisons_pk PRIMARY KEY (id),
+   CONSTRAINT fk_livraisons_utilisateurs FOREIGN KEY(utilisateur) REFERENCES utilisateurs(id) ON UPDATE NO ACTION ON DELETE CASCADE,
+   CONSTRAINT fk_livraisons_boites FOREIGN KEY(boite) REFERENCES boites(id) ON UPDATE NO ACTION ON DELETE CASCADE
+);
+ALTER TABLE livraisons OWNER to "admin";
 
-/****************HISOTRY*****************************/
-CREATE TABLE public.history
-(
-    id integer NOT NULL DEFAULT nextval('history_id_seq'::regclass),
-    door integer NOT NULL,
-    users integer NOT NULL,
-    date character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    action integer NOT NULL,
-    CONSTRAINT history_pkey PRIMARY KEY (id),
-    CONSTRAINT history_users_fkey FOREIGN KEY (users)
-        REFERENCES public.users (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE CASCADE
-)
+/****************Acces********************/
 
-TABLESPACE pg_default;
-
-ALTER TABLE public.history
-    OWNER to "postgresArnaud";
-
-/******************USERS***************************/
-CREATE TABLE public.users
-(
-    id integer NOT NULL DEFAULT nextval('users_id_seq'::regclass),
-    firstname character varying COLLATE pg_catalog."default",
-    lastname character varying COLLATE pg_catalog."default",
-    phone character varying COLLATE pg_catalog."default",
-    sexe character(1) COLLATE pg_catalog."default",
-    mail character varying COLLATE pg_catalog."default",
-    password character varying COLLATE pg_catalog."default",
-    isadmin boolean,
-    CONSTRAINT users_pkey PRIMARY KEY (id)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE public.users
-    OWNER to "postgresArnaud";
+CREATE TABLE acces (
+   utilisateur  INTEGER       		NOT NULL,    
+   boite  	INTEGER       		NOT NULL,            
+   nom  	VARCHAR(1000) 		NOT NULL,
+   CONSTRAINT acces_pk PRIMARY KEY (utilisateur,boite),
+   CONSTRAINT fk_acces_utilisateurs FOREIGN KEY(utilisateur) REFERENCES utilisateurs(id) ON UPDATE NO ACTION ON DELETE CASCADE,
+   CONSTRAINT fk_acces_boites FOREIGN KEY(boite) REFERENCES boites(id) ON UPDATE NO ACTION ON DELETE CASCADE
+);
+ALTER TABLE boites OWNER to "admin";
