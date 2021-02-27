b3b3ee93d2edd03531981c1d5c94c3459e4abd59
Damien Delestienne <he201749@students.ephec.be>
2021-02-27 20:18:56 +0100

    maj sql

--- damien@51.91.102.255/Postgre/creaDB.sql
+++ damien@51.91.102.255/Postgre/creaDB.sql
@@ -8,6 +8,7 @@ CREATE TABLE utilisateurs(
    mdp          varchar(1000)     	NOT NULL,
    CONSTRAINT utilisateurs_pk PRIMARY KEY (id)
 );
+ALTER TABLE utilisateurs OWNER to "admin";
 
 /****************Boites********************/
 
@@ -17,6 +18,7 @@ CREATE TABLE boites (
    ip           varchar(1000)     	NOT NULL,
    CONSTRAINT boites_pk PRIMARY KEY (id)
 );
+ALTER TABLE boites OWNER to "admin";
 
 /****************Livraisons********************/
 
@@ -33,6 +35,7 @@ CREATE TABLE livraisons (
    CONSTRAINT fk_livraisons_utilisateurs FOREIGN KEY(utilisateur) REFERENCES utilisateurs(id) ON UPDATE NO ACTION ON DELETE CASCADE,
    CONSTRAINT fk_livraisons_boites FOREIGN KEY(boite) REFERENCES boites(id) ON UPDATE NO ACTION ON DELETE CASCADE
 );
+ALTER TABLE livraisons OWNER to "admin";
 
 /****************Acces********************/
 
@@ -44,3 +47,4 @@ CREATE TABLE acces (
    CONSTRAINT fk_acces_utilisateurs FOREIGN KEY(utilisateur) REFERENCES utilisateurs(id) ON UPDATE NO ACTION ON DELETE CASCADE,
    CONSTRAINT fk_acces_boites FOREIGN KEY(boite) REFERENCES boites(id) ON UPDATE NO ACTION ON DELETE CASCADE
 );
+ALTER TABLE boites OWNER to "admin";
